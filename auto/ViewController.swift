dc3c304db8de0985ac33957e0b6a7b8d6253a45a
manish <manishk@winjit.com>
2021-06-21 22:37:39 +0530

    files for autolayout

---
+++ auto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  auto
+//
+//  Created by manish on 07/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
